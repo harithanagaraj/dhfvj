3cb749bd2c4f7197df5e658d55c988cede29d80f
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-26 16:04:30 +0530

    prgmn
    good

---
+++ muli.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int n, i;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+
+    for(i=1; i<=10; ++i)
+    int(i=0;i++;i>9);
+    {
+        printf("%d * %d = %d \n", n, i, n*i);
+    }
+    
+    return 0;
+}
